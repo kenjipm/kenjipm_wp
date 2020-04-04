23fdd92aa0d8da24935d9c78fc7f6ef178c8f00c
Kenji Prahyudi Mudjono <kenji@rekadia.co.id>
2020-04-04 22:08:23 +0700

    Update footer.php

--- wp-content/themes/gutenbook-child/footer.php
+++ wp-content/themes/gutenbook-child/footer.php
@@ -14,19 +14,9 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer">
-		<!--div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'gutenbook' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'gutenbook' ), esc_html__( 'WordPress', 'gutenbook' ) );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'gutenbook' ), 'gutenbook', ' <a href="https://odiethemes.com"> ' . esc_html__( 'OdieThemes', 'gutenbook' ) . '</a>' );
-				?>
-		</div-->
+		<div class="site-info">
+			
+		</div>
 		<!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
