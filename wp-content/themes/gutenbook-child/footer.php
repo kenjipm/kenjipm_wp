c009d56ad782e51dd0a458423c47265f573b2e4e
Kenji Prahyudi Mudjono <kenji@rekadia.co.id>
2020-04-04 21:56:56 +0700

    footer mod moved to child theme

--- wp-content/themes/gutenbook-child/footer.php
+++ wp-content/themes/gutenbook-child/footer.php
@@ -14,7 +14,7 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer">
-		<div class="site-info">
+		<!--div class="site-info">
 			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'gutenbook' ) ); ?>">
 				<?php
 				/* translators: %s: CMS name, i.e. WordPress. */
@@ -26,7 +26,8 @@
 				/* translators: 1: Theme name, 2: Theme author. */
 				printf( esc_html__( 'Theme: %1$s by %2$s.', 'gutenbook' ), 'gutenbook', ' <a href="https://odiethemes.com"> ' . esc_html__( 'OdieThemes', 'gutenbook' ) . '</a>' );
 				?>
-		</div><!-- .site-info -->
+		</div-->
+		<!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
